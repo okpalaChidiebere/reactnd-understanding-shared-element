dfda94099086598798eac2789b16b7b7cd25f2e2
okpalaChidiebere <okpalacollins4@gmail.com>
2021-07-25 22:00:14 -0600

    Demo 2 - Fix SharedElement transition bug
    - we made a mistake of wraping the the whole StoryThumbnail with the SharedElement. This will not yield in a smooth transiton. You must wrap the direct child View you want to perform the trnasition on and not the Parent View.

--- src/components/StoryThumbnail.js
+++ src/components/StoryThumbnail.js
@@ -17,24 +17,24 @@ export default function StoryThumbnail({ story }){
 
     useFocusEffect(() => {
         if (navigation.isFocused()) {
-        setOpacity(1)
+            setOpacity(1) //once this screen is back on focus by the user navigating back, we make the listItem the user visited to be visible again! 
         }
     })
 
     return (
-        <SharedElement id={story.id}>
-            <Pressable
+        <Pressable
             style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
             onPress={() => {
                 setOpacity(0);
                 navigation.navigate(Strings.component_snapchat_stories_details, { story })
             }}
-            >
-                <View style={[styles.container, /*{ opacity }*/]}>
+        >
+            <View style={[styles.container, { opacity /** Because the detailScreen is has transparent background, you can see mainScreen through. So when the user clicks on an item, we set the opacity to 0, this will let the user know the listItem tha they are viewing */}]}>
+                <SharedElement id={story.id} style={{ flex: 1 }}>
                     <Image source={story.source} style={styles.image} />
-                </View>
-            </Pressable>
-        </SharedElement>
+                </SharedElement>
+            </View>
+        </Pressable>
     )
 }
 
