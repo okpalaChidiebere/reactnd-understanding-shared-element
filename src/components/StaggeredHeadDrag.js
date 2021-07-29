3ee78c30b11ff40b9b4ae48debae5bec121261ab
okpalaChidiebere <okpalacollins4@gmail.com>
2021-07-29 16:41:34 -0600

    Staggered Head Drag - Set up the drag for the first tiem

--- src/components/StaggeredHeadDrag.js
+++ src/components/StaggeredHeadDrag.js
@@ -34,6 +34,8 @@ export default function StaggeredHeadDrag({ }){
 
     const [heads, setHeads] = useState(initialState)
 
+    const AnimatedImageBackground = Animated.createAnimatedComponent(ImageBackground)
+
     /** 
      * When we were breaking down the animation, we noticed that we will have to so some sort of drag
      * This means we will set up a PanResonder and it is only going to be a single PanResponder for our first
@@ -43,8 +45,27 @@ export default function StaggeredHeadDrag({ }){
         PanResponder.create({
             onStartShouldSetPanResponder: (e, gestureState) => true,
             onMoveShouldSetPanResponder: (evt, gestureState) => true,
-            onPanResponderGrant: (e, gestureState) => {},
-            onPanResponderMove: (e, gestureState) => {},
+            onPanResponderGrant: (e, gestureState) => {
+                /** 
+                 * Since we know the View you are dragging is going to stay where its at after being dragged we will need to use an extract offset.
+                 * We are going to need to map over all the animated values and extract the offSet for each one 
+                 * */
+                heads.map(({ animation }) => {
+                    /**
+                     * Reset Animated.ValueXY to 0. This will keep your finger and the top head together as you drag. 
+                     * if you dont do this there will be an increasing space between your finger and the item you are dragging while you drag. 
+                     * We dont want that
+                     */
+                    animation.extractOffset()
+                })
+            },
+            onPanResponderMove: (e, { dx, dy }) => {
+                //We only want to setValues for the first Head.
+                heads[0].animation.setValue({
+                    x: dx,
+                    y: dy,
+                })
+            },
             // onPanResponderRelease: (e, gestaureState) => {} //Since mo release animation, so we dont need this
         })
     ).current
@@ -52,18 +73,31 @@ export default function StaggeredHeadDrag({ }){
     return (
         <SafeAreaView style={styles.container} edges={["bottom", "left", "right"]}>
             <View style={styles.container}>
-                {heads.slice(0).reverse().map((item, index, items) => {
+                {heads
+                .slice(0) //shallow clone the heads
+                .reverse() //revers the list so the first head will actually appear on top so that we can drag it around. The did this same thing for KittenCards
+                .map((item, index, items) => {
+
+                    //Beacuse the  item we want to drag is at the end, we have to chack for it
+                    const pan = index === items.length - 1 
+                    ? panResponder.panHandlers : {} //NOTE: we returned an empty object so that we can continue the spread on all of the images
                     return (
-                        <ImageBackground
-                            key={index}
+                        <AnimatedImageBackground
+                            {...pan}
+                            key={index /**Remember ideally you want to use Ids. But for now this will do */}
                             source={item.image}
                             //imageStyle={{ borderRadius: 40 }}
-                            style={[styles.head]}
+                            style={[
+                                styles.head,
+                                {
+                                    transform: item.animation.getTranslateTransform() //craft the translateXY for us
+                                }
+                            ]}
                         >
                         <Text>
                             {item.text}
                         </Text>
-                        </ImageBackground>
+                        </AnimatedImageBackground>
                     );
                 })}
             </View>
