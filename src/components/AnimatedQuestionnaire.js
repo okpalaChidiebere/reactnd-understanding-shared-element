2f986001d06c640bac0f38a084820d7b47018238
okpalaChidiebere <okpalacollins4@gmail.com>
2021-07-31 21:26:27 -0600

    Animated Questionnaire - Add Buttons

--- src/components/AnimatedQuestionnaire.js
+++ src/components/AnimatedQuestionnaire.js
@@ -23,9 +23,29 @@ const initialState = {
 export default function AnimatedQuestionnaire(){
     const [state, setState] = useState(initialState)
 
+    const handleAnswer = () => {}
+
     return (
         <SafeAreaView style={styles.container} edges={["bottom", "left", "right"]}>
-            <View style={styles.content}></View>
+            <View style={styles.content}>
+                <TouchableOpacity 
+                    /**
+                     * TIP: TouchableOpacity is actually an Animated.View so that means 
+                     * we can apply styling to it. It is still in a column direction. */
+                    onPress={handleAnswer} 
+                    activeOpacity={.7} 
+                    style={styles.option}
+                >
+                    <Text style={styles.optionText}>No</Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    onPress={handleAnswer} 
+                    activeOpacity={.7}
+                    style={[styles.option, styles.yes]} 
+                >
+                    <Text style={styles.optionText}>Yes</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -38,6 +58,19 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: "#E22D4B",
       flexDirection: "row", //this will allow us to lay our buttons out on the left and right sides
+    },
+    option: {
+        flex: 1, //we want the button stretch the entire screen
+        justifyContent: "flex-end", //we want our content to be at the end
+        alignItems: "center" //we want our content to be centered inside
+    },
+    yes: {
+        backgroundColor: "rgba(255,255,255,.1)", //we want to distinguish between the yes and no button
+    },
+    optionText: {
+        fontSize: 30,
+        color: "#FFF",
+        marginBottom: 50,
     },
 })
 
