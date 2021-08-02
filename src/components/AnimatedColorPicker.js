533e114e43fbfab172609aefc327e8a9b53e2461
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-02 15:35:49 -0600

    Animated Color Picker - Set up the views involved in animating
    - We Add The Other Buttons
    - And the two content views (actionView and inputView) set on top of each other

--- src/components/AnimatedColorPicker.js
+++ src/components/AnimatedColorPicker.js
@@ -8,13 +8,19 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
 } from "react-native"
+import { Foundation } from "@expo/vector-icons"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Colors, Strings } from "../values"
 
 
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput)
+const AnimatedIcon = Animated.createAnimatedComponent(Foundation)
+
 const initialState = {
     color: "#000", //default color to black to drive the color in the input View
-    
+    /**We use this to control pointer events in the for the inputView. The inputs 
+     * View has an absoluteFill style that makes it display over the ActionView */
+    inputOpen: false, 
 }
 export default function AnimatedColorPicker({ }){
 
@@ -37,7 +43,7 @@ export default function AnimatedColorPicker({ }){
 
     const handleToggle = () => {}
 
-    const { color } = state
+    const { color, inputOpen } = state
 
     /** we want the background color of the colorAction button to be driven off our state */
     const colorStyle = {
@@ -51,7 +57,39 @@ export default function AnimatedColorPicker({ }){
                     <TouchableWithoutFeedback>
                         <Animated.View style={[styles.colorBall, colorStyle]}/>
                     </TouchableWithoutFeedback>
-                    <View style={styles.row}></View>
+                    <View style={styles.row}>
+                        <TouchableOpacity>
+                            <AnimatedIcon name="bold" size={30} color="#555" />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <AnimatedIcon name="italic" size={30} color="#555" />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <AnimatedIcon
+                                name="align-center"
+                                size={30}
+                                color="#555"
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <AnimatedIcon name="link" size={30} color="#555" />
+                        </TouchableOpacity>
+
+                        <Animated.View /** The inputView position on top of the actionView( with actionIcons) */
+                            style={[StyleSheet.absoluteFill, styles.colorRowWrap]}
+                        >
+                            <AnimatedTextInput
+                                value={color}
+                                style={[styles.input]}
+                                onChangeText={(color) => setState(currstate => ({ ...currstate, color }))}
+                            />
+                            <TouchableWithoutFeedback /*onPress={this.toggleInput}*/>
+                                <Animated.View style={[styles.okayButton]}>
+                                    <Text style={styles.okayText}>OK</Text>
+                                </Animated.View>
+                            </TouchableWithoutFeedback>
+                        </Animated.View>
+                    </View>
                 </Animated.View>
                 <TouchableOpacity onPress={handleToggle} style={styles.button}>
                     <Text>Toggle Open/Closed</Text>
@@ -93,8 +131,31 @@ const styles = StyleSheet.create({
         borderRadius: 8 //makes it a perfect circle
     },
     row: {
-
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-around", //there will be even spaces around the actionView icons
+        flexDirection: "row",
+        overflow: "hidden",
     },
+    colorRowWrap: {
+        flexDirection: "row",
+        flex: 1,
+        paddingLeft: 5,
+    },
+    input: {
+        flex: 1,
+    },
+    okayButton: {
+        borderRadius: 20,
+        width: 33,
+        height: "100%",
+        backgroundColor: "#309eeb",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    okayText: {
+        color: "#fff",
+    }
 })
 
 export function AnimatedColorPickerOptions({ }){
