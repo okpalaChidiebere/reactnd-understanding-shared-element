d224f39b73955bdb51955903d170dfc918a33e08
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-01 18:21:16 -0600

    Photo Grid Shared Element - Build a Grid
    Now we are setup to receive whatever image that as clicked on and then we can do our measurements and transition

--- src/assets/avatars/Heroes.js
+++ src/assets/avatars/Heroes.js
@@ -1,4 +1,4 @@
-export const Heroes = [
+export default [
     {
       id: "saitama",
       name: "Saitama",
--- src/components/PhotoGridSharedElement.js
+++ src/components/PhotoGridSharedElement.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useRef } from "react"
 import {
   StyleSheet,
   Text,
@@ -11,14 +11,53 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Colors, Strings } from "../values"
 
-import Heros from "../assets/avatars/Heroes"
+import Heroes from "../assets/avatars/Heroes"
 
 export default function PhotoGridSharedElement({ }){
+
+    /** 
+     * we want to save all the references to all images in the grid
+     * 
+     * When you press on the image, we will dynamically measure the 
+     * element that you pressed and receive a callback with its positioning.
+     * 
+     * With the help of refs we can do that
+    */
+    const gridImages = useRef([])
+
+    /**
+     * 
+     * @param {*} index index for the image that was clicked so that we 
+     * can do our measurements and transition for that particular image
+     */
+    const handleOpenImage = (index) => {}
 
     return (
         <SafeAreaView style={styles.container} edges={["bottom", "left", "right"]}>
             <View style={styles.container}>
-                
+                <ScrollView style={styles.container
+                    /** takses up the available space. FYI: we could use a FlatList 
+                     * if you're worried about performance, but the concepts still apply. */}>
+                    <View style={styles.grid}>
+                        {
+                            Heroes.map(({ id, photo }, index) => {
+                                return (
+                                    <TouchableWithoutFeedback
+                                        key={id}
+                                        onPress={() => handleOpenImage(index)}
+                                    >
+                                        <Image
+                                            ref={image => gridImages.current[index] = image}
+                                            source={photo}
+                                            style={[styles.gridImage /**we want grid sized images */]}
+                                            resizeMode="cover"
+                                        />
+                                    </TouchableWithoutFeedback>
+                                )
+                            })
+                        }
+                    </View>
+                </ScrollView>
             </View>
         </SafeAreaView>
     )
@@ -27,6 +66,14 @@ export default function PhotoGridSharedElement({ }){
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+    },
+    grid: {
+        flexDirection: "row",
+        flexWrap: "wrap", //if we dont add this the images will overflow off the screen horizontally
+    },
+    gridImage: {
+        width: "33%", //we want our images to be three per row
+        height: 150, //ideally, you will want to calculate this based upon screen size
     },
 })
 
