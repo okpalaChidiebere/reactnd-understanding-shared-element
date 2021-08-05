116b82f4642cba42dea657bbab3d855d336f62ae
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-05 10:13:50 -0600

    Application Intro - Set up Our ScrollView
    We set up our scrollView to fill up the screen.
    ** Pay attention to the props we used to configured for the scrollView to act like an app screen

--- src/components/ApplicationIntro.js
+++ src/components/ApplicationIntro.js
@@ -1,20 +1,34 @@
-import React, {  } from "react"
+import React, { useRef } from "react"
 import {
   StyleSheet,
   Text,
   View,
-  Animated,
-  ScrollView,
   Dimensions,
   PixelRatio,
 } from "react-native"
+import Animated, { useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Colors, Strings } from "../values"
 
 export default function ApplicationIntro(){
+    const animation = useSharedValue(0)
+
     return(
         <SafeAreaView style={styles.container} edges={["bottom", "left", "right"]}>
-            <View style={styles.container}></View>
+            <View style={styles.container}>
+                <Animated.ScrollView
+                    style={styles.container} /** we want the scrollView to fill up the outside space*/
+                    pagingEnabled /** makes pages based upon the width and the height of the scrollView. In our case, its the whole screen*/
+                    horizontal /**enable horizonatal scroll for this */
+                    scrollEventThrottle={16}
+                    onScroll={useAnimatedScrollHandler(e => {
+                        //because we are scrolling horizontally we choose the x offset
+                        animation.value = e.contentOffset.x
+                    })}
+                >
+                    
+                </Animated.ScrollView>
+            </View>
         </SafeAreaView>
     )
 }
