0924db5b2db14bcd6b9f972d44ec03a2770f5140
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-07 21:19:30 -0600

    Horizontal Parallax ScrollView - Starter Code 2
    - Add the pagedScrollView
    - Add the title of the image in the middle of the screen for each image

--- src/components/HorizontalParallaxScroll.js
+++ src/components/HorizontalParallaxScroll.js
@@ -4,10 +4,15 @@ import {
   StyleSheet,
   Text,
   View,
+  Dimensions,
 } from "react-native"
+import Animated, { useSharedValue, useAnimatedScrollHandler } from "react-native-reanimated"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Colors, Strings } from "../values"
+import Moment from "./Moment"
 
+
+const { width, height } = Dimensions.get("window") //returns the current dimensions of the phone screen
 const Images = [
     { image: require("../assets/stories/1.jpg") , title: "Vodca Cran" },
     { image: require("../assets/stories/2.jpg"), title: "Old Fashion" },
@@ -16,10 +21,28 @@ const Images = [
 ]
 
 export default function HorizontalParallaxScroll(){
+    const animatedScroll = useSharedValue(0)
     return (
         <SafeAreaView style={styles.container} edges={["bottom", "left", "right"]}>
             <View style={[styles.container]}>
-
+                <Animated.ScrollView
+                    pagingEnabled //cause the scrollView to stop at multiples of its width
+                    horizontal
+                    scrollEventThrottle={16}
+                    onScroll={useAnimatedScrollHandler(e => {
+                        //because we are scrolling horizontally we choose the x offset
+                        animatedScroll.value = e.contentOffset.x
+                    })}
+                >
+                    {Images.map((image, i) => {
+                        return (
+                            <Moment 
+                              key={i}
+                              {...image} //pass on the image properties as props "image" and "title"
+                            />
+                        )
+                    })}
+                </Animated.ScrollView>
             </View>
         </SafeAreaView>
     )
---
+++ src/components/Moment.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import Animated from "react-native-reanimated"
+const { width, height } = Dimensions.get("window")
+
+export default function Moment({ title, image }){
+    return (
+        <View style={styles.container}>
+            <Animated.Image
+                source={image}
+                style={[styles.image]}
+                resizeMode="cover"
+            />
+            <View style={[StyleSheet.absoluteFill, styles.center]}>
+                <View style={styles.textWrap}>
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+            </View>
+        </View>
+      );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width,
+    height,
+    overflow: "hidden"
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null
+  },
+  center: {
+    justifyContent: "center"
+  },
+  textWrap: {
+    backgroundColor: "rgba(0,0,0,.5)",
+    paddingVertical: 10
+  },
+  title: {
+    backgroundColor: "transparent",
+    fontSize: 30,
+    color: "#FFF",
+    textAlign: "center"
+  }
+})
