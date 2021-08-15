203d05a1e30ddd58dc6ba8e6bf83052b3e9af93d
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-14 21:56:18 -0600

    Updated README file and SVG Morph in Reanimated2

--- src/components/AnimatedSvg.js
+++ src/components/AnimatedSvg.js
@@ -1,11 +1,19 @@
-import React, { useRef, useEffect } from "react"
+import React from "react"
 import {
   StyleSheet,
   View,
-  Animated,
   TouchableWithoutFeedback,
-  Easing
 } from "react-native"
+import Animated, { 
+    useSharedValue, 
+    runOnJS, 
+    withTiming,  
+    useAnimatedProps, 
+    withSequence, 
+    withDelay,
+    Easing,
+} from "react-native-reanimated"
+import { interpolatePath, parse } from "react-native-redash" //you must import react-native-reanimated bfore you import react-native-redash
 import { SafeAreaView } from "react-native-safe-area-context"
 import Svg, { Path } from "react-native-svg"
 import { interpolate } from "flubber" //fLubber is used for smoother animations in between SVG paths https://github.com/veltman/flubber
@@ -15,52 +23,61 @@ import { Colors, Strings } from "../values"
 const ic_tick = `M4.8,13.4 L9,17.6 M10.4,16.2 L19.6,7`
 const ic_cross = `M6.4,6.4 L17.6,17.6 M6.4,17.6 L17.6,6.4`
 
+const shape1 = parse("M4.8,13.4 L9,17.6 M10.4,16.2 L19.6,7")
+const shape2 = parse("M6.4,6.4 L17.6,17.6 M6.4,17.6 L17.6,6.4")
+
+
+const AnimatedPath = Animated.createAnimatedComponent(Path)
+
 export default function AnimatedSvg({ }){
 
     const startPath = ic_tick
     const endPath = ic_cross
-    const _path = useRef() //helps us to be able to call setNativeProps() method
-    const animation = useRef(new Animated.Value(0)).current
-    /**
-     * Animated does not support anmating SVG paths; however flubber interpolate does
-     * So we can just pass the two strings we want to interpolate between and it do the interpolation for us.
-     * interpolate from flubber has the ability to
-     */
-    const pathInterpolate = interpolate(startPath, endPath, {
-        maxSegmentLength: 1,
+    const animation = useSharedValue(0)
+    const d = useSharedValue()
+
+    /** 
+     * because  interpolate from flubber is an external library and it is not marked as worklet like
+     * interpolatePath from react-native-redash, we will must use a callback to run this code and pass this
+     * callback to  runOnJS() to be able to run this code in reanimatedV2 hooks
+    */
+    const handleInterpolatePath = (progress) => {
+        const pathInterpolate = interpolate(startPath, endPath, {
+            maxSegmentLength: 1,
+        })
+        d.value = pathInterpolate(progress.value)
+    }
+
+    /**SIDE NOTE: 
+    * Setting the props(with setNativeProps) is how animated works.
+    * Using setNativeProps we can by pass setState. every single React-native components have this function as props
+    * 
+    * In our case, it makesense to use setNativeProps to update that props dynamically
+    * useAnimatedProps() from reanimatedV2 uses the setNativeProps behind the scenes!
+    */
+    const animatedProps = useAnimatedProps(() => {
+        /**
+         * Animated does not support anmating SVG paths; however flubber interpolate react-native-redash interplatePath does
+         * 
+         * For flubber we can just pass the two strings we want to interpolate between and it do the interpolation for us.
+         * interpolate from flubber has the ability to
+         * 
+         * For interpolatePath method works well when you want to morph between more than two svg paths
+         * this method is already worklet but for some reason, the cross_tick path was not drawn properly.
+         * I will have to look into mastering basics of SVG
+         */
+        runOnJS(handleInterpolatePath)(animation)
+        return {
+          d: d.value,
+          //d: interpolatePath(animation.value, [0, 1], [shape1, shape2])
+        }
     })
 
-    useEffect(() => {
-        animation.addListener(({ value }) => {
-          const path = pathInterpolate(value)
-
-          /**
-           * Setting the props(with setNativeProps) is how animated works.
-           * Using setNativeProps we can by pass setState. every single React-native components have this function as props
-           * 
-           * In our case, it makesense to use setNativeProps to update that props dynamically
-           */
-          _path.current.setNativeProps({ d: path }) //remeber the Path component have the d props passed to it
-        })
-        return () => animation.removeAllListeners()//remove the listener once this comonent unmoints
-    }, [])
-
     const handlePress = () => {
-        Animated.sequence([
-          Animated.timing(animation, {
-            toValue: 1,
-            duration: 300,
-            easing: Easing.linear, //optional. see more https://codedaily.io/courses/Master-React-Native-Animations/Easing
-            useNativeDriver: true,
-          }),
-          Animated.delay(1500),
-          Animated.timing(animation, {
-            toValue: 0,
-            duration: 300,
-            easing: Easing.bounce, //optional
-            useNativeDriver: true,
-          }),
-        ]).start()
+        animation.value = withSequence(
+            withTiming(1, { duration: 300, easing: Easing.linear }),
+            withDelay(1500, withTiming(0, { duration: 300 })),
+        )
     }
 
     return (
@@ -68,11 +85,10 @@ export default function AnimatedSvg({ }){
             <View style={styles.container}>
                 <TouchableWithoutFeedback onPress={handlePress}>
                     <Svg width={120} height={120}>
-                        <Path
+                        <AnimatedPath
                             scale={3}
-                            d={startPath}
                             stroke="black"
-                            ref={_path}
+                            animatedProps={animatedProps}
                             /** Addtional props to correctly have the tick icon look good*/
                             strokeWidth={2}
                             strokeLinecap="square"
